e23d877dd4d4ff35858656abc996805f18ff6c02
plok74122 <plok74122@hotmail.com>
2016-03-16 00:14:15 +0800

    修正User關聯course的名稱

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ActiveRecord::Base
   belongs_to :group
-  has_many :group_of_courses , :through => :group , :source => :courses , class_name: "course"
+  # has_many :group_of_courses , :through => :group , :source => :courses , class_name: "course"
   # has_many :group_of_courses, class_name: "Course", foreign_key: "group_id"
   has_many :check_in_lists
-  has_many :courses, :through => :check_in_lists
+  has_many :course, :through => :check_in_lists
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
